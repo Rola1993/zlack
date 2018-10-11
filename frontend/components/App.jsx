eb8b47f3b8d63503a030d5faa9969abb1d17e8ce
Rola1993 <rola.uiuc@gmail.com>
2018-10-10 17:43:16 -0700

    test

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -18,7 +18,7 @@ const homeContainer = () => {
       <div className="homeTxt">
           <h1>Where Work Happens</h1>
           <p>When your team needs to kick off a project, hire a new employee, deploy some code, review a sales contract, finalize next year's budget, measure an A/B test, plan your next office opening, and more, Slack has you covered.</p>
-          <span> <span>
+          <span> </span>
     </div>
     </div>
 
