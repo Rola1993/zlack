aa4e178bd1d295ce53c06470c0e2878340df2b49
Rola1993 <rola.uiuc@gmail.com>
2018-11-28 12:49:23 -0500

    message model update

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ApplicationRecord
+  validates :body, presence:true
 
   belongs_to :user
+  belongs_to :channel
   
   after_create_commit do
     MessageCreationEventBroadcastJob.perform_later(self)
